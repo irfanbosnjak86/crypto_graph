4ff821261c360ca7a4a47ccea21f056870756f89
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-08-09 15:31:47 +0200

    Adds Currency model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809120529) do
+ActiveRecord::Schema.define(version: 20170809132550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.integer  "currency_type"
+    t.float    "value"
+    t.date     "date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,7 @@
 FactoryGirl.define do
+  factory :currency do
+    currency_type 1
+    value 1.5
+    date "2017-08-09"
+  end
 end
